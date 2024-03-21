454539ad68fa2b2adf90db59310e299666592831
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2024-03-21 01:53:03 +0100

    ignore shift if ctrl pressed

--- docs/gb/gb.js
+++ docs/gb/gb.js
@@ -96,23 +96,26 @@ class GameBoy {
           }
           break;
         case "Shift":
-          if (!event.ctrlKey && event.location == KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
-            this.wasm.instance.exports.press_select();
+          if (!event.ctrlKey) {
+            if (event.location == KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+              this.wasm.instance.exports.press_select();
+            }
+
+            if (!event.repeat) {
+              // NOTE: this is needed as keyUp wont fire for other keys if Shift is pressed
+              this.wasm.instance.exports.release_a();
+              this.wasm.instance.exports.release_b();
+              this.repeat_keys.delete("KeyS");
+              this.repeat_keys.delete("KeyW");
+              this.repeat_keys.delete("KeyA");
+              this.repeat_keys.delete("KeyQ");
+
+              this.startSec = audioCtx.currentTime + this.audio_latency;
+              this.turbo = false;
+              this.fast_mode = this.last_mode;
+            }
           }
 
-          if (!event.repeat) {
-            // NOTE: this is needed as keyUp wont fire for other keys if Shift is pressed
-            this.wasm.instance.exports.release_a();
-            this.wasm.instance.exports.release_b();
-            this.repeat_keys.delete("KeyS");
-            this.repeat_keys.delete("KeyW");
-            this.repeat_keys.delete("KeyA");
-            this.repeat_keys.delete("KeyQ");
-
-            this.startSec = audioCtx.currentTime + this.audio_latency;
-            this.turbo = false;
-            this.fast_mode = this.last_mode;
-          }
           break;
         case "x":
           this.wasm.instance.exports.press_a();
